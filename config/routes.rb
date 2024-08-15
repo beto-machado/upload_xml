bef1faa4e9e064564023963553b85eafe17fc7e8
beto-machado <luisrobertobeto3@gmail.com>
2024-08-15 19:36:38 -0300

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+require "sidekiq/web"
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  username == 'admin' && password == 'password'
+end
+
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
   # root "posts#index"
+  
+  mount Sidekiq::Web, at: "sidekiq"
 end
